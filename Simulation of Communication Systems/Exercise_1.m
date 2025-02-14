f53c31496cb8d44c79f26cc3cc4d9daabf70ad1d
Brauuwu <survivalminecraft080@gmail.com>
2025-02-14 22:28:00 +0700

    Add files via upload

---
+++ Simulation of Communication Systems/Exercise_1.m
@@ -0,0 +1,10 @@
+% Exercise 1
+M = [1 0 0; 0 1j 1; 1j 1j+1 -3]     % Define Matrix 3x3 M
+
+k = 2.75    % Define Variable k
+
+v = [1; 3; -7; -0.5]    % Define Column Vector v
+
+w = [1 -5.5 -1.7 -1.5 3 -10.7]  % Define Row Vector w
+
+y = 1 : 0.5 : 100.5     % Define Row Vector y (Step=0.5)
