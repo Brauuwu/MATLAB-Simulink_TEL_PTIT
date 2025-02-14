c884e9f8000691c77614bb9584da68b1847f2b7a
Brauuwu <survivalminecraft080@gmail.com>
2025-02-14 23:34:56 +0700

    Add files via upload

---
+++ Simulation of Communication Systems/Exercise_3.m
@@ -0,0 +1,7 @@
+% Exercise 3
+
+A = [-1 3.5 2; 0 1 -1.3; 1.1 2 1.9]     % Define Matrix 3x3 A
+
+B = [1 0 -1; -1.5 -1.5 -3; 1 1 1]       % Define Matrix 3x3 B
+
+res = A*B   % Product of Matrix A and Matrix B
